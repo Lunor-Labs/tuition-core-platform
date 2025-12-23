976131689e41cbdc2e724b880eb0c18795e022e6
Dinesh Sadaruwan <imdineshsandaru@gmail.com>
2025-12-23 09:01:36 +0530

    Updated the login and register pages

--- src/features/landing/pages/LoginPage.tsx
+++ src/features/landing/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './AuthPage.css';
 import LoginForm from '../components/Auth/LoginForm';
+import studentBg from '../../../assets/register/student-bg.jpg';
 
 const LoginPage: React.FC = () => {
   const handleLoginSuccess = () => {
@@ -10,12 +11,26 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className="auth-page">
-      <div className="auth-wrapper">
-        <h2>Login</h2>
-        <LoginForm 
-          onSwitchToRegister={() => { window.location.href = '/register'; }} 
-          onLoginSuccess={handleLoginSuccess}
-        />
+      {/* Left Side - Image */}
+      <div className="auth-left">
+        <div className="auth-image-container">
+          <img src={studentBg} alt="Student" className="auth-image" />
+          <div className="auth-image-overlay">
+            <h2 className="auth-overlay-title">Lorem Ipsum is simply</h2>
+            <p className="auth-overlay-subtitle">Lorem Ipsum is simply</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Right Side - Form */}
+      <div className="auth-right">
+        <div className="auth-wrapper">
+          <h2>Login</h2>
+          <LoginForm 
+            onSwitchToRegister={() => { window.location.href = '/register'; }} 
+            onLoginSuccess={handleLoginSuccess}
+          />
+        </div>
       </div>
     </div>
   );
--- src/features/landing/pages/RegisterPage.tsx
+++ src/features/landing/pages/RegisterPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './AuthPage.css';
 import RegisterForm from '../components/Auth/RegisterForm';
+import studentBg from '../../../assets/register/student-bg.jpg';
 
 const RegisterPage: React.FC = () => {
   const handleRegisterSuccess = () => {
@@ -10,12 +11,26 @@ const RegisterPage: React.FC = () => {
 
   return (
     <div className="auth-page">
-      <div className="auth-wrapper">
-        <h2>Register</h2>
-        <RegisterForm 
-          onSwitchToLogin={() => { window.location.href = '/login'; }} 
-          onRegisterSuccess={handleRegisterSuccess}
-        />
+      {/* Left Side - Image */}
+      <div className="auth-left">
+        <div className="auth-image-container">
+          <img src={studentBg} alt="Student" className="auth-image" />
+          <div className="auth-image-overlay">
+            <h2 className="auth-overlay-title">Lorem Ipsum is simply</h2>
+            <p className="auth-overlay-subtitle">Lorem Ipsum is simply</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Right Side - Form */}
+      <div className="auth-right">
+        <div className="auth-wrapper">
+          <h2>Register</h2>
+          <RegisterForm 
+            onSwitchToLogin={() => { window.location.href = '/login'; }} 
+            onRegisterSuccess={handleRegisterSuccess}
+          />
+        </div>
       </div>
     </div>
   );
